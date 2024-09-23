30ee483cb8b1093b5c0b382186e0ef82ea40a308
FabricioSilvany <fabriciosilvany57@gmail.com>
2024-09-23 17:03:29 +0000

    Teste de versionamento

--- 7. Listas  -  Vetores/Teste de versionamento - 2.py
+++ 7. Listas  -  Vetores/Teste de versionamento - 2.py
@@ -2,5 +2,18 @@ import os
 
 os.system("cls || clear")
 
-#Comandos
-#
+"""Indentificar arquivos para salvar (commit)"""
+#git status
+
+"""Adicionar arquivos para salvar(commit)"""
+#git add .
+
+"""Realizando salvamento (commit)"""
+#git commit -m "Nome" 
+
+"""Atualizar seu repositório local em
+relação ao repositório remoto"""
+#git pull
+
+"""Envia seu código para seu repositório remoto"""
+#git push
