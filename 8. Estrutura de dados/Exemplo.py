e3aa7832550de585acbad8328c4fdf389fec8762
FabricioSilvany <fabriciosilvany57@gmail.com>
2024-09-26 18:05:24 +0000

    Estruturas de dados

--- 8. Estrutura de dados/Exemplo.py
+++ 8. Estrutura de dados/Exemplo.py
@@ -26,3 +26,24 @@ print("\n=== Exibindo dados dos alunos ===")
 for aluno in lista_de_alunos:
     print(f"Nome: {aluno.nome}")
     print(f"Idade: {aluno.idade}")
+
+
+
+#Definido arquivo para salvar os dados
+nome_do_arquivo = "Lista_de_alunos_SENAI.txt"
+
+#Abrindo arquivo e definindo que será feita a escrita de dados.
+with open(nome_do_arquivo, "w") as arquivo_alunos:
+#Percorredo vetor/lista.    
+    for aluno in lista_de_alunos:
+#Escrevendi no arquivo uma linha de cada vez.        
+        arquivo_alunos.write(f"{aluno.nome}, {aluno.idade}\n")
+
+print("\n=== Dados dos alunos salvo com sucesso! ===")
+
+#CRUD
+
+#[X]CREATE
+#READ
+#UPDATE
+#DELETE
